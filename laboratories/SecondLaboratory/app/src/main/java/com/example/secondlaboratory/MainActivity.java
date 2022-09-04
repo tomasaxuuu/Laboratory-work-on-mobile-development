6d6de7d29afb0d580f6c6a48a91f16b18b456440
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-09-04 19:33:22 +0400

    first ver secondLab XML

---
+++ laboratories/SecondLaboratory/app/src/main/java/com/example/secondlaboratory/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.secondlaboratory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+//    Button choice = findViewById(R.id.choice);
+//    Button reset = findViewById(R.id.reset);
+//    Button output = findViewById(R.id.output);
+//    Button add = findViewById(R.id.addItem);
+//    EditText newItem = findViewById(R.id.editItem);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
