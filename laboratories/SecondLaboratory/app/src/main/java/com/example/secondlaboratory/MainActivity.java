47f4855a745a02fa9a758dcc2f014923a03a6505
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-09-05 19:45:04 +0400

    +добавление в список + очистка editText + скрытие клавиатуры

--- laboratories/SecondLaboratory/app/src/main/java/com/example/secondlaboratory/MainActivity.java
+++ laboratories/SecondLaboratory/app/src/main/java/com/example/secondlaboratory/MainActivity.java
@@ -2,14 +2,44 @@ package com.example.secondlaboratory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editItem = findViewById(R.id.editItem);
+        Button addItem = findViewById(R.id.addItem);
+        ListView list = findViewById(R.id.list);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_item, arrayList);
+
+        list.setAdapter(adapter);
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.add(editItem.getText().toString());
+
+                // скрытие клавиатуры
+                if (view != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+
+                // очистка editText
+                editItem.getText().clear();
+            }
+        });
     }
 }
