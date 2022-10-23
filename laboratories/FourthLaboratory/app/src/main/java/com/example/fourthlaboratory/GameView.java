c7b79ae26727f7f90fccdbf28b2126c540b6633c
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-10-23 14:04:39 +0400

    the maze is drawn + touches on the screen are processed for movement in the maze + dialogs about the results + newLvl upload

--- laboratories/FourthLaboratory/app/src/main/java/com/example/fourthlaboratory/GameView.java
+++ laboratories/FourthLaboratory/app/src/main/java/com/example/fourthlaboratory/GameView.java
@@ -11,20 +11,21 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class GraphView extends View {
+public class GameView extends View {
 
-    Paint black = new Paint();
-    Paint red = new Paint();
-    Paint yellow = new Paint();
-    Paint greenYellow = new Paint();
-    Paint blue = new Paint();
+    Paint wallBlack = new Paint();
+    Paint playerRed = new Paint();
+    Paint yellowField = new Paint();
+    Paint trapGreen = new Paint();
+    Paint blueFinish = new Paint();
+    Paint teleportPurple = new Paint();
 
     private int x = 1;
     private int y = 1;
     private float arrSize, hMargin, wMargin;
     private int h = 10;
     private int w = 10;
-    private int[][] arr = {
+    private int[][] arrLvl1 = {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
             {1, 0, 1, 0, 4, 0, 1, 0, 0, 1},
@@ -36,28 +37,53 @@ public class GraphView extends View {
             {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
+    private int[][] arrLvl2 = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 2, 1},
+            {1, 0, 0, 4, 1, 1, 1, 1, 0, 1},
+            {1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 4, 1, 1, 4, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 2, 1, 1, 1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 3, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
 
     private enum Direction {
         UP, DOWN, LEFT, RIGHT
     }
 
-    public GraphView(Context context) {
+    public GameView(Context context) {
         super(context);
     }
 
-    public GraphView(Context context, @Nullable AttributeSet attrs) {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    private void newLvl() {
+        arrLvl1 = arrLvl2;
+        x = 1;
+        y = 8;
+    }
+
+    private void lostLvl() {
+        arrLvl2 = arrLvl1;
+        x = 1;
+        y = 1;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        red.setColor(Color.rgb(255, 0, 0));
-        black.setColor(Color.rgb(0, 0, 0));
-        greenYellow.setColor(Color.rgb(0, 128, 0));
-        yellow.setColor(Color.rgb(189, 183, 107));
-        blue.setColor(Color.rgb(0, 0, 255));
+        playerRed.setColor(Color.rgb(255, 0, 0));
+        wallBlack.setColor(Color.rgb(0, 0, 0));
+        trapGreen.setColor(Color.rgb(0, 128, 0));
+        yellowField.setColor(Color.rgb(189, 183, 107));
+        blueFinish.setColor(Color.rgb(0, 0, 255));
+        teleportPurple.setColor(Color.rgb(146,110,174));
 
         float width = getWidth();
         float height = getHeight();
@@ -76,98 +102,127 @@ public class GraphView extends View {
         // рисование лабиринта
         for (int j = 0; j < h; j++) {
             for (int i = 0; i < w; i++) {
-                switch (arr[j][i]) {
+                switch (arrLvl1[j][i]) {
                     // field
                     case 0:
-                        canvas.drawCircle(i * 70, j * 70, 30, yellow);
+                        canvas.drawCircle(i * 70, j * 70, 30, yellowField);
                         break;
                     // wall
                     case 1:
-                        canvas.drawCircle(i * 70, j * 70, 30, black);
+                        canvas.drawCircle(i * 70, j * 70, 30, wallBlack);
                         break;
                     // finish
                     case 2:
-                        canvas.drawCircle(i * 70, j * 70, 30, blue);
+                        canvas.drawCircle(i * 70, j * 70, 30, blueFinish);
+                        break;
+                    // teleport
+                    case 3:
+                        canvas.drawCircle(i * 70, j * 70, 30, teleportPurple);
                         break;
                     // trap
                     case 4:
-                        canvas.drawCircle(i * 70, j * 70, 30, greenYellow);
+                        canvas.drawCircle(i * 70, j * 70, 30, trapGreen);
                         break;
                     }
             }
 
         }
-        canvas.drawCircle(x * 70, y * 70, 25, red);
+        canvas.drawCircle(x * 70, y * 70, 25, playerRed);
     }
 
     private void movePlayer(Direction direction) {
-        // сообщение о результате
+
+        // message about result
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        // победа
+        // victory
         builder.setTitle(R.string.victoryTitle);
         builder.setMessage(R.string.victoryMessage);
         AlertDialog victoryDialog = builder.create();
-        // поражение
+        // lost
         builder.setTitle(R.string.lostTitle);
         builder.setMessage(R.string.lostMessage);
         AlertDialog lostDialog = builder.create();
+        // teleport
+        builder.setTitle(R.string.teleportTitle);
+        builder.setMessage(R.string.teleportMessage);
+        AlertDialog teleportDialog = builder.create();
 
         switch (direction) {
             case UP:
-                if (arr[y - 1][x] == 0) {
+                if (arrLvl1[y - 1][x] == 0) {
                     y--;
                 }
-                else if (arr[y - 1][x] == 4) {
+                else if (arrLvl1[y - 1][x] == 4) {
                     lostDialog.show();
-                    y = 1;
-                    x = 1;
+                    lostLvl();
                 }
-                else if (arr[y - 1][x] == 2) {
+                else if (arrLvl1[y - 1][x] == 2) {
                     victoryDialog.show();
                     y--;
-
+                    newLvl();
+                }
+                else if (arrLvl1[y - 1][x] == 3) {
+                    teleportDialog.show();
+                    y = 1;
+                    x = 1;
                 }
                 break;
+
             case DOWN:
-                if (arr[y + 1][x] == 0) {
+                if (arrLvl1[y + 1][x] == 0) {
                     y++;
                 }
-                else if (arr[y + 1][x] == 4) {
+                else if (arrLvl1[y + 1][x] == 4) {
                     lostDialog.show();
-                    y = 1;
-                    x = 1;
+                    lostLvl();
                 }
-                else if (arr[y + 1][x] == 2) {
+                else if (arrLvl1[y + 1][x] == 2) {
                     victoryDialog.show();
                     y++;
+                    newLvl();
+                }
+                else if (arrLvl1[y + 1][x] == 3) {
+                    teleportDialog.show();
+                    y = 1;
+                    x = 1;
                 }
                 break;
             case LEFT:
-                if (arr[y][x - 1] == 0) {
+                if (arrLvl1[y][x - 1] == 0) {
                     x--;
                 }
-                else if (arr[y][x - 1] == 4) {
+                else if (arrLvl1[y][x - 1] == 4) {
                     lostDialog.show();
-                    y = 1;
-                    x = 1;
+                    lostLvl();
                 }
-                else if (arr[y][x - 1] == 2) {
+                else if (arrLvl1[y][x - 1] == 2) {
                     victoryDialog.show();
                     x--;
+                    newLvl();
+                }
+                else if (arrLvl1[y][x - 1] == 3) {
+                    teleportDialog.show();
+                    y = 1;
+                    x = 1;
                 }
                 break;
             case RIGHT:
-                if (arr[y][x + 1] == 0) {
+                if (arrLvl1[y][x + 1] == 0) {
                     x++;
                 }
-                else if (arr[y][x + 1] == 4) {
+                else if (arrLvl1[y][x + 1] == 4) {
                     lostDialog.show();
-                    y = 1;
-                    x = 1;
+                    lostLvl();
                 }
-                else if (arr[y][x + 1] == 2) {
+                else if (arrLvl1[y][x + 1] == 2) {
                     victoryDialog.show();
                     x++;
+                    newLvl();
+                }
+                else if (arrLvl1[y][x + 1] == 3) {
+                    teleportDialog.show();
+                    y = 1;
+                    x = 1;
                 }
                 break;
         }
@@ -185,13 +240,10 @@ public class GraphView extends View {
 
             float x1 = event.getX();
             float y1 = event.getY();
-
-            float playerCenterX = hMargin + (arr[0][x] + 0.5f) * arrSize;
-            float playerCenterY = wMargin + (arr[y][0] + 0.5f) * arrSize;
-
+            float playerCenterX = hMargin + (arrLvl1[0][x] + 0.5f) * arrSize;
+            float playerCenterY = wMargin + (arrLvl1[y][0] + 0.5f) * arrSize;
             float dx = x1 - playerCenterX;
             float dy = y1 - playerCenterY;
-
             float absDx = Math.abs(dx);
             float absDy = Math.abs(dy);
 
